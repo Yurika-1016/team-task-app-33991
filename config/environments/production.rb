fa2f6fcf410eeabb76e18546d53c9a47a0ac803d
Yurika-1016 <cix11103@nitech.jp>
2021-03-24 12:28:37 +0900

    ActionCableの本番環境デプロイに関わる修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,8 +40,9 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  ActionCable.server.config.disable_request_forgery_protection = true
+   config.action_cable.url = 'wss://team-task-app-33991.herokuapp.com/cable'
+   config.action_cable.allowed_request_origins = [ 'https://team-task-app-33991.herokuapp.com', 'https://team-task-app-33991.herokuapp.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
