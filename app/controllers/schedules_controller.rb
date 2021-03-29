bd5d43343e7c6224d7920be4bc7ad03d31d3be87
Yurika-1016 <cix11103@nitech.jp>
2021-03-29 09:39:51 +0900

    リファクタリング

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -3,8 +3,11 @@ class SchedulesController < ApplicationController
 
   def index
     @users = User.all
+    # @scheduleで今日で登録してあるスケジュールを取得（テーブルがUTCのためyesterdayで取得）
     @schedules = Schedule.where('date >= ?', Date.yesterday)
+    # 今月で登録されている勤務時間(totalテーブルデータ)を取得
     @totals = Total.where(date: Time.current.all_month)
+    # start_timeが今日で登録されているデータを取得
     @tasks = Task.where(start_time: Time.current.all_day)
   end
 
---
+++ app/javascript/header.js
@@ -0,0 +1,44 @@
+window.addEventListener('load', function (){
+  const headerBtn1 = document.getElementById("header-btn1");
+  headerBtn1.addEventListener('mouseover', function(){
+    headerBtn1.setAttribute("style", "color: tan")
+  })
+  headerBtn1.addEventListener('mouseout', function(){
+    headerBtn1.removeAttribute("style", "color: tan")
+  })
+
+  const headerBtn2 = document.getElementById("header-btn2");
+  headerBtn2.addEventListener('mouseover', function(){
+    headerBtn2.setAttribute("style", "color: darkgray")
+  })
+  headerBtn2.addEventListener('mouseout', function(){
+    headerBtn2.removeAttribute("style", "color: darkgray")
+  })
+
+  const headerBtn3 = document.getElementById("header-btn3");
+  headerBtn3.addEventListener('mouseover', function(){
+    headerBtn3.setAttribute("style", "color: red")
+  })
+  headerBtn3.addEventListener('mouseout', function(){
+    headerBtn3.removeAttribute("style", "color: red")
+  })
+})
+
+window.addEventListener('load', function (){
+  const headerBtn4 = document.getElementById("header-btn4");
+  headerBtn4.addEventListener('mouseover', function(){
+    headerBtn4.setAttribute("style", "color: tan")
+  })
+  headerBtn4.addEventListener('mouseout', function(){
+    headerBtn4.removeAttribute("style", "color: tan")
+  })
+
+  const headerBtn5 = document.getElementById("header-btn5");
+  headerBtn5.addEventListener('mouseover', function(){
+    headerBtn5.setAttribute("style", "color: darkgray")
+  })
+  headerBtn5.addEventListener('mouseout', function(){
+    headerBtn5.removeAttribute("style", "color: darkgray")
+  })
+
+})
